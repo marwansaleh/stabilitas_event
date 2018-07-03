9a1d76379707dc497d89e5c1449c070eaa269d59
IT01 <IT01@IT01-PC>
2018-07-03 09:34:10 +0700

    Add CI engine

---
+++ codeigniter/application/core/Home.php
@@ -0,0 +1,18 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Home
+ *
+ * @author IT01
+ */
+class Home extends MY_Controller {
+    public function index() {
+        $this->load->view('', $this->data);
+    }
+}
---
+++ codeigniter/application/core/MY_Controller.php
@@ -0,0 +1,20 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of MY_Controller
+ *
+ * @author IT01
+ */
+class MY_Controller extends CI_Controller {
+    protected $data = [];
+    
+    public function __construct() {
+        parent::__construct();
+    }
+}
